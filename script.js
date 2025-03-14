9a2b82de0e4c67b7058dab2e4bea45ab02a349b9
01aaa01 <abdunnurmusoxonov213@gmail.com>
2025-03-14 22:28:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('Sizda hozir random sonlar chiqishni boshlaydi😉')
+
+alert('Random orqali chiqqan son: 55')
+
+alert("Biz random sonni 2ga ko'paytirmoqchi va bo'lmoqchimiz !")
+
+
+    alert("Random orqali chiqqan son: 55 edi")
+    alert(`biz uni 2ga ko'paytirib 110 sonini chiqardik,`)
+    alert(`va shu sonni 2ga bo'lib: 27.5 sonini chiqardik,`)
+    alert(`va shu sonni 3ga bo'lgandagi qoldiq: 1 sonini chiqardik`)
